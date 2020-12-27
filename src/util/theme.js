ab99c696d6803196ff0d7150ca498dc0ec8664c2
unknown <aaulia.raahman@gmail.com>
2020-12-27 08:15:22 +0800

    react

---
+++ src/util/theme.js
@@ -0,0 +1,103 @@
+export default {
+    palette: {
+      primary: {
+        light: '#33c9dc',
+        main: '#00bcd4',
+        dark: '#008394',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#ff6333',
+        main: '#ff3d00',
+        dark: '#b22a00',
+        contrastText: '#fff'
+      }
+    },
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: 'center'
+    },
+    image: {
+      margin: '20px auto 20px auto',
+      width: '65px',
+      height: '65px'
+    },
+    pageTitle: {
+      margin: '10px auto 10px auto'
+    },
+    textField: {
+      margin: '10px auto 10px auto'
+    },
+    button: {
+      marginTop: 20,
+      position: 'relative'
+    },
+    customError: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginTop: 10
+    },
+    progress: {
+      position: 'absolute'
+    },
+    buttonEditDetails: {
+      float: 'right'
+    },
+    submitButton: {
+      position: 'relative',
+      float: 'right',
+      marginTop: 10
+    },
+    progressSpinner: {
+      position: 'absolute'
+    },
+    closeButton: {
+      position: 'absolute',
+      left: '91%',
+      top: '6%'
+    },
+    invisibleSeparator: {
+      border: 'none',
+      margin: 4
+    },
+    visibleSeparator: {
+      width: '100%',
+      borderBottom: '1px solid rgb(101,108,124)',
+      marginBottom: 20
+    },
+    visibleSeparatorSecond: {
+      width: '100%',
+      borderBottom: '1px rgb(191,194,199)',
+      marginBottom: 20
+    },
+    profileImage: {
+       maxWidth: 200,
+       height: 200,
+       borderRadius: '50%',
+       objectFit: 'cover'
+    },
+    dialogContent: {
+      padding: 20
+    },
+    expandButton: {
+      position: 'absolute',
+      left: '90%'
+    },
+    spinnerDiv: {
+      textAlign: 'center',
+      marginTop: 50,
+      marginBottom: 50
+    },
+    commentImage: {
+      maxWidth: '100%',
+      height: 100,
+      objectFit: 'cover',
+      borderRadius: '50%'
+    },
+    commentData: {
+      marginLeft: 20
+    }
+
+}
